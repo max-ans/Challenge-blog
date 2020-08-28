581027b80d7652e9ac8850440f4044c14e6bf078
max-ans <maxenceroyer@hotmail.com>
2020-08-28 18:19:23 +0200

    React-model

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,11 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
   </div>
 );
